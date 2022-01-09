67063134356d114cfd084b2500bae15a9632330a
mr Nanders0n <owner@mail>
2022-01-10 00:52:39 +0300

    add article

--- _site/assets/search.js
+++ _site/assets/search.js
@@ -1,1 +1,1 @@
-window.TEXT_SEARCH_DATA={'posts':[{'title':"Post with Header Image",'url':"/2018/06/01/header-image.html"},{'title':"Welcome",'url':"/2018/07/01/welcome.html"}]};
+window.TEXT_SEARCH_DATA={'posts':[{'title':"Post with Header Image",'url':"/2018/06/01/header-image.html"},{'title':"О чём этот блог",'url':"/2022/01/10/about_blog.html"}]};
