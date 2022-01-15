3c0ee92159ab7b675d17c8b7767da08a70052269
mr Nanders0n <owner@mail>
2022-01-15 15:14:42 +0300

    added header images and new article

--- _site/assets/search.js
+++ _site/assets/search.js
@@ -1,1 +1,1 @@
-window.TEXT_SEARCH_DATA={'posts':[{'title':"О чём этот блог",'url':"/2022/01/10/about_blog.html"}]};
+window.TEXT_SEARCH_DATA={'posts':[{'title':"О чём этот блог",'url':"/2022/01/10/about_blog.html"},{'title':"Мой лайфхак для прохождения интервью",'url':"/2022/01/15/my_interviews_experience.html"}]};
